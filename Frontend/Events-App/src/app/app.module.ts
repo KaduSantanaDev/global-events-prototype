965a137219ded157e99d12ceaf54529350d6f211
kadu <kaduzera.ks@gmail.com>
2022-01-15 20:04:55 -0300

    Nav bar front end

--- Frontend/Events-App/src/app/app.module.ts
+++ Frontend/Events-App/src/app/app.module.ts
@@ -7,17 +7,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { PanelistsComponent } from './panelists/panelists.component';
+import { NavComponent } from './nav/nav.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse'
 
 @NgModule({
   declarations: [
     AppComponent,
       EventsComponent,
-      PanelistsComponent
+      PanelistsComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
