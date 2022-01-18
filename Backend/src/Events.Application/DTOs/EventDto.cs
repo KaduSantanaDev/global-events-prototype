deac9deb4f2c7fa2c73eb267c0cc7facaee175ff
kadu <kaduzera.ks@gmail.com>
2022-01-18 01:11:40 -0300

    Implement Data Annotations in DTO class

--- Backend/src/Events.Application/DTOs/EventDto.cs
+++ Backend/src/Events.Application/DTOs/EventDto.cs
@@ -9,10 +9,15 @@ namespace Events.Application.DTOs
         public int Id { get; set; }
         public string Local { get; set; }
         public DateTime? DateEvent { get; set; }
+        [Required, StringLength(50, MinimumLength = 4)]
         public string Theme { get; set; }
+        [Required, Range(10, 120000)]
         public int NumPeople { get; set; }
+        [RegularExpression(@".*\.(gif|jpe?g|bmp|png|jfif)$")]
         public string UrlImage { get; set; }
+        [Required, Phone]
         public string Phone { get; set; }
+        [Required, EmailAddress]
         public string Email { get; set; }
 
         public IEnumerable<BatchDto> Batches { get; set; }
