3d3ba37d81a5ac3c0f782f9ddcb203e8209cd0a0
kadu <kaduzera.ks@gmail.com>
2022-01-18 04:33:36 -0300

    Change Event Component Layout

--- Frontend/Events-App/src/app/shared/title/title.component.ts
+++ Frontend/Events-App/src/app/shared/title/title.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
   @Input() title: string = ''
+  @Input() iconClass = 'fa fa-user'
+  @Input() subtitle = 'Since 2022'
+  @Input() listButton = false
   constructor() { }
 
   ngOnInit() {
